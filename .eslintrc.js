e3386b025514b64b6c3147f14857f4f174a738d6
Frank Bauer <developer@ambertation.de>
2021-04-09 14:49:57 +0200

    linting and latest version of hap-controller

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'no-multi-spaces': ['error']
+    }
+}
