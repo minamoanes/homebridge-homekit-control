cbb061c130bd22cecce62133d3f3ec139d721faf
Frank Bauer <developer@ambertation.de>
2021-04-09 23:31:09 +0200

    Added FakeGato
    Prevent value updates for serial
    
    Bugfix
    
    Added Custom AirQuality Characteristic for Eve
    
    Version up

--- HomeKitPlatform.js
+++ HomeKitPlatform.js
@@ -1,9 +1,9 @@
 'use strict'
-const {
-    HttpClient
-} = require('hap-controller')
+const {HttpClient} = require('hap-controller'),
+    inherits = require('util').inherits
+    
 
-let Service, Characteristic, KnownServiceMap, KnownCharMap
+let Service, Characteristic, KnownServiceMap, KnownCharMap, FakeGatoService, CustomCharacteristic
 
 function shortType(type){
     const parts = type.split('-')
@@ -82,7 +82,8 @@ function buildCharMap(){
         Characteristic.Hue,
         Characteristic.ColorTemperature,
         Characteristic.Saturation,
-        Characteristic.CurrentAmbientLightLevel
+        Characteristic.CurrentAmbientLightLevel,
+        Characteristic.AirParticulateDensity
     ]
 
     //mapping to the final data structure
@@ -108,6 +109,7 @@ class HKClient {
     constructor(serviceConfig, parent) {
         this.parent = parent     
         this.serviceConfig = serviceConfig  
+        this.deviceID = serviceConfig.name
         
         //state
         this.didFinishWasDefered = false
@@ -116,8 +118,17 @@ class HKClient {
 
         //data
         this.supportedAccessories = []    
-        this.uuid = parent.api.hap.uuid.generate(serviceConfig.name)
-        this.name = serviceConfig.name
+        if (serviceConfig.uniquePrefix){
+            this.uuid = parent.api.hap.uuid.generate(`${serviceConfig.uniquePrefix}.${serviceConfig.name}`)
+        } else {
+            this.uuid = parent.api.hap.uuid.generate(serviceConfig.name)
+        }
+        this.name = serviceConfig.name?serviceConfig.name:'Unnamed Accessory'
+        this.fakeGato = { 
+            users:[],
+            interval: 0,
+            timer: undefined
+        }
 
         const self = this
 
@@ -148,7 +159,7 @@ class HKClient {
                     }),
                     source:s
                 }
-            })) 
+            }))             
                     
 
             if (this.didFinishWasDefered) {
@@ -158,7 +169,7 @@ class HKClient {
         }).catch((e) => {
             self.parent.log.error('Error', e)
         })
-       
+
 
         parent.api.on('didFinishLaunching', () => {
             if (self.accesoriesWereLoaded) {
@@ -167,7 +178,11 @@ class HKClient {
                 this.didFinishWasDefered = true
             }
         })
-    }    
+
+        if (serviceConfig.historyInterval > 0){
+            this.fakeGato.interval = Math.max(30, Math.min(600, serviceConfig.historyInterval))            
+        }
+    }  
 
     con(){
         return new HttpClient(
@@ -196,55 +211,83 @@ class HKClient {
                                         
         char.setProps(props)
     }
+
+    _updateCharacteristicValue(char, c){
+        if (!c.allowValueUpdates) {return}
+        this.con().getCharacteristics(
+            [c.cname],
+            {
+                meta: true,
+                perms: true,
+                type: true,
+                ev: true,
+            }
+        ).then((data) => {
+            c.value = data.characteristics[0].value
+            this.parent.log('Received Value', c.cname, c.value)
+            
+            char.updateValue(c.value)                                
+        }).catch((e) => this.parent.log.error('get', c.cname, e))
+    }
+
+    _setCharacteristicValue(char, value, c){
+        const data = {}
+        data[c.name] = value
+                    
+        this.con().setCharacteristics(data).then(() => {
+            c.value = value 
+            this.parent.log('Wrote Value', c.cname, c.value)                           
+        }).catch((e) => this.parent.log.error('set', c.cname, e))
+    }
     
     initAccessoryService(sData, service){
         const self = this
 
-        sData.characteristics.forEach((c) => {                    
-            service.setCharacteristic(c.create, c.source.value)
+        sData.characteristics.forEach((c) => {   
+            c.allowValueUpdates = true
+            if (c.create.UUID === Characteristic.SerialNumber.UUID && this.serviceConfig.uniquePrefix){
+                this.deviceID = c.value
+                c.allowValueUpdates = false                
+                c.value = `${this.serviceConfig.uniquePrefix}-${this.deviceID}`
+            } else if (c.create.UUID === Characteristic.Name.UUID && this.serviceConfig.name){
+                c.allowValueUpdates = false                
+                c.value = this.serviceConfig.name
+            } 
+
+            if (c.source.value !== undefined) {
+                service.setCharacteristic(c.create, c.value)
+            }
             const char = service.getCharacteristic(c.create)
+            c.connect = char
             self.copyProps(c.source, char)
+
+            
+            
             
             if (c.source.perms && c.source.perms.indexOf('pr')>=0){                                                
-                char.on('get', (callback) => {
-                    this.con().getCharacteristics(
-                        [c.cname],
-                        {
-                            meta: true,
-                            perms: true,
-                            type: true,
-                            ev: true,
-                        }
-                    ).then((data) => {
-                        c.value = data.characteristics[0].value
-                        char.updateValue(c.value)                                
-                    }).catch((e) => this.parent.log.error('get', c.cname, e))
-
+                char.on('get', (callback) => {                    
+                    self._updateCharacteristicValue(char, c)                                        
                     callback(null, c.value)
                 })                                            
             }
 
             if (c.source.perms && (c.source.perms.indexOf('pw')>=0 | c.source.perms.indexOf('tw')>=0)){
                 char.on('set', (value, callback) => {
-                    const data = {}
-                    data[c.name] = value
-                    
-                    this.con().setCharacteristics(data).then(() => {
-                        c.value = value                            
-                    }).catch((e) => this.parent.log.error('set', c.cname, e))
-
+                    self._setCharacteristicValue(char, value, c)                    
                     callback(null)
                 })
             }
 
-            if (c.source.perms && (c.source.perms.indexOf('ev')>=0)){
+            if (c.allowValueUpdates && c.source.perms && (c.source.perms.indexOf('ev')>=0)){
                 const cl = this.con()
                 cl.on('event', event => {
                     if (event.characteristics && event.characteristics.length>0){
                         const data = event.characteristics[0]
                         
                         if (c.cname === `${data.aid}.${data.iid}` && data.value) {
-                            {this.parent.log.debug('Got Event', c.cname, data.value)}
+                            this.parent.log.debug(`Got Event for ${c.cname}. Changed value ${c.value} => ${data.value}`)
+
+                            c.value = data.value
                             char.updateValue(data.value)                                
                         }
                     }
@@ -255,8 +298,9 @@ class HKClient {
                     c.connection = conn                            
                 }).catch((e) => console.error(e))
             }                    
-        })
+        })        
     }
+
 
     loadOrCreate(accessoryServices){
         const self = this
@@ -267,6 +311,7 @@ class HKClient {
         if (configuredAcc === undefined) {
             // create a new accessory
             const accessory = new this.parent.api.platformAccessory(this.name, this.uuid)
+            this.parent.log.debug(`Building new Accessory ${accessory.displayName} (${accessory.UUID})`)
             accessoryServices.map(sData => {
                 let service = accessory.getService(sData.create)
 
@@ -276,10 +321,12 @@ class HKClient {
                 
                 self.initAccessoryService(sData, service)                
             })
+            self.addFakeGatoService(accessoryServices.fakeGato, accessory)
 
             // register the accessory
             this.parent.api.registerPlatformAccessories('homebridge-homekit-controller', 'HomeKitController', [accessory])            
         } else {
+            this.parent.log.debug(`Reconfiguring existing Accesory ${configuredAcc.displayName} (${configuredAcc.UUID})`)
             accessoryServices.map(sData => {
                 let service = configuredAcc.getService(sData.create)
 
@@ -287,34 +334,135 @@ class HKClient {
                     service = configuredAcc.addService(sData.create)
                 }
                 
-                self.initAccessoryService(sData, service)                
+                self.initAccessoryService(sData, service)
             })
+            self.addFakeGatoService(accessoryServices.fakeGato, configuredAcc)
+
             configuredAcc._wasUsed = true
         }
-
-
-        // const uuid = this.parent.api.hap.uuid.generate(accessoryServices.uname)
-        // // console.log(uuid, accessoryItem.uname)
     }
 
     didFinishLaunching() {  
-        const self = this  
+        const self = this   
+        if (this.fakeGato.interval > 0){
+            this.amendFakeGato()
+        }       
 
-        this.supportedAccessories.forEach(acc => self.loadOrCreate(acc))  
-        // const uuid = this.api.hap.uuid.generate('SOMETHING UNIQUE');
-
-        // // check the accessory was not restored from cache
-        // if (!this.accessories.find(accessory => accessory.UUID === uuid)) {
-
-        //     // create a new accessory
-        //     const accessory = new this.api.platformAccessory('DISPLAY NAME', uuid);
-
-        //     // register the accessory
-        //     this.api.registerPlatformAccessories('PLUGIN_NAME', 'PLATFORM_NAME', [accessory]);
-        // }
-
+        this.supportedAccessories.forEach(acc => self.loadOrCreate(acc))          
         this.didFinishStartup = true
         this.parent.clientFinishedLaunching(self)
+
+        if (this.fakeGato.interval > 0) {
+            this.parent.log(`Enabeling History Log for '${this.name}' every ${this.fakeGato.interval} seconds`)            
+            
+            this.fakegatoEvent()
+            this.fakeGato.timer = setInterval(self.fakegatoEvent.bind(self), this.fakeGato.interval*1000)
+        }
+    }  
+
+    amendFakeGato(){
+        if (this.fakeGato.interval<=0) {return}
+        this.supportedAccessories.forEach(acc =>{
+            const has = function(type, cType) {
+                const s = acc.find(s => s.create.UUID === type.UUID)
+                if (cType && s){
+                    return s.characteristics.find(c => c.create.UUID == cType.UUID)
+                }
+                return s
+            }
+
+            acc.fakeGato = {
+                room:{
+                    temp:has(Service.TemperatureSensor, Characteristic.CurrentTemperature),
+                    humidity:has(Service.HumiditySensor, Characteristic.CurrentRelativeHumidity),
+                    ppm:[has(Service.CarbonDioxideSensor, Characteristic.CarbonDioxideLevel), has(Service.AirQualitySensor, Characteristic.PM2_5Density), has(Service.AirQualitySensor, Characteristic.PM10Density), has(Service.CarbonMonoxideSensor, Characteristic.CarbonMonoxideLevel), ].find(i=>i!==undefined)
+                },
+                motion:{
+                    status:has(Service.MotionSensor, Characteristic.MotionDetected)
+                }
+            }
+
+            acc.fakeGato.history = []
+            if (acc.fakeGato.room.temp && acc.fakeGato.room.humidity && acc.fakeGato.room.ppm){
+                acc.fakeGato.history.push({'type':'room', data:acc.fakeGato.room, logService:undefined})
+            } else if (acc.fakeGato.room.temp && acc.fakeGato.room.humidity){
+                acc.fakeGato.history.push({'type':'weather', data:{temp:acc.fakeGato.room.temp, humidity:acc.fakeGato.room.humidity}, logService:undefined})
+            } else if (acc.fakeGato.motion.status){
+                acc.fakeGato.history.push({'type':'motion', data:acc.fakeGato.motion, logService:undefined})
+            }
+        })
+    }
+
+
+    addFakeGatoService(fakeGato, accessory){
+        //add the fakeGato service if needed
+        if (fakeGato && this.fakeGato.interval > 0){            
+            accessory.log = this.parent.log
+            fakeGato.history.forEach(h => {
+                const airQualityService = accessory.getService(Service.AirQualitySensor)
+                
+                if (airQualityService && h.type==='room') {                                       
+                    let airQualityC = airQualityService.getCharacteristic(CustomCharacteristic.AirQuality)
+                    
+                    let valueGetter = () => {
+                        if(h.data.ppm.UUID === Characteristic.PM2_5Density || h.data.ppm.UUID === Characteristic.PM10Density){
+                            //sems to be ug/m3 not ppm => convert like https://github.com/simont77/fakegato-history/issues/107
+                            return Math.max(500, h.data.ppm.value / 4.57)
+                        } else {
+                            return Math.max(500, h.data.ppm.value)
+                        }
+                    }
+                    if (airQualityC===undefined) {
+                        this.parent.log.debug(`Adding Eve PPM Characteristic to '${this.name}', ${this.deviceID}, ${this.serviceConfig.uniquePrefix?this.serviceConfig.uniquePrefix:''} = ${valueGetter()}`)
+                        airQualityC = airQualityService.setCharacteristic(CustomCharacteristic.AirQuality, valueGetter())
+                    } else {
+                        this.parent.log.debug(`Updating initial value for Eve PPM Characteristic to ${valueGetter()}`)
+                        airQualityC.updateValue(valueGetter())
+                    }
+
+                    airQualityC.onGet(async ()=> valueGetter())
+                }
+                
+                h.logService = accessory.getService(FakeGatoService)
+                if (h.logService === undefined){
+                    this.parent.log.debug(`Adding FakeGatoService '${h.type}' to '${this.name}', ${this.deviceID}, ${this.serviceConfig.uniquePrefix?this.serviceConfig.uniquePrefix:''}`)
+                    h.logService = new FakeGatoService(h.type, accessory, {size:4000,disableTimer:true})                    
+                }
+            })
+        }
+    }
+
+    fakegatoEvent(){
+        const self = this
+        this.parent.log('Timer - ', this.name)
+
+        this.supportedAccessories.filter(acc=> acc.fakeGato && acc.fakeGato.history.length>0).forEach(acc => {
+            acc.fakeGato.history.forEach(h => {
+                const data = {
+                    time: Math.round(new Date().valueOf() / 1000)
+                }
+                Object.keys(h.data).forEach(k=> {
+                    const c = h.data[k]
+                    
+                    if (k==='ppm') {
+                        data[k] = Math.max(500, c.value)
+                    } else {
+                        data[k] = c.value
+                    }
+                    
+                    //no auto updates, so trigger manual reads
+                    //if (c.source.perms && (c.source.perms.indexOf('ev')<0))
+                    {
+                        this.parent.log('FakeGato Triggered Value read', c.cname)
+                        self._updateCharacteristicValue(c.connect, c)
+                    }
+                })
+                
+                console.log(h.type, data)
+                h.logService.addEntry(data)
+            })
+            
+        })
     }
 }
 
@@ -323,10 +471,19 @@ class HKPlatform {
         this.config = config
         this.api = api
         this.log = log
+        
         this.accessories = []
-
-        const self = this
-        this.clients = this.config.services.map(serviceConfig => new HKClient(serviceConfig, self))        
+        const self = this        
+        
+        this.clients = this.config.services===undefined?[]:this.config.services.map(serviceConfig => new HKClient(serviceConfig, self))  
+        
+        this.api.on('didFinishLaunching', () => {
+            if (this.clients.length===0){
+                this.clientFinishedLaunching(undefined)
+            }  
+        })
+         
+                
     }
 
     /**
@@ -334,28 +491,58 @@ class HKPlatform {
      * accessory restored
      */
     configureAccessory(accessory) {
+        this.log.debug(`Loaded Accesory ${accessory.displayName} (${accessory.UUID})`)
         accessory._wasUsed = false
         this.accessories.push(accessory)
     }
 
     clientFinishedLaunching(cl){
         const allReady = this.clients.map(c => c.didFinishStartup).filter(ok => !ok).length===0
-        this.log(`${cl.name} finished Startup. ${allReady?'All Clients ready now':''}`)
+        if (cl!==undefined){
+            this.log(`${cl.name} finished Startup. ${allReady?'All Clients ready now':''}`)
+        } else if (allReady) {
+            this.log.debug('All Clients ready now')
+        }
 
         if (allReady){
+            this.log.debug('Loaded Accesories')
+            this.log.debug(this.accessories.map(a => ` - [${a._wasUsed?'X':' '}] ${a.displayName} (${a.UUID})`).join('\n'))
             const unused = this.accessories.filter(a => a._wasUsed===false)
             this.log(`Found ${unused.length} unused accessories`)
-            this.log(unused)
+            
             if (process.argv.some(v=>v==='-Q')){
-                this.log('Deleting unused accessories')
-            }
+                this.log('Deleting unused accessories')  
+                unused.forEach(a => this.api.unregisterPlatformAccessories('homebridge-homekit-controller', 'HomeKitController', [a]))
+            }            
         }
     }
 }
 
-module.exports = function (service, characteristic) {
+module.exports = function (service, characteristic, homebridge) {
     Service = service
     Characteristic = characteristic
+    FakeGatoService = require('fakegato-history')(homebridge)
+
+
+    
+    CustomCharacteristic={}
+    CustomCharacteristic.AirQuality = function () {
+        Characteristic.call(this, 'Air Quality PM25', 'E863F10B-079E-48FF-8F27-9C2605A29F52')
+        this.setProps({
+            format: Characteristic.Formats.UINT16,
+            unit: 'ppm',
+            maxValue: 99999,
+            minValue: 500,
+            minStep: 1,
+            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+        })
+        this.value = this.getDefaultValue()
+    }
+    CustomCharacteristic.AirQuality.UUID = 'E863F10B-079E-48FF-8F27-9C2605A29F52'
+    CustomCharacteristic.AirQuality.uuid = 'E863F10B-079E-48FF-8F27-9C2605A29F52'
+    
+    inherits(CustomCharacteristic.AirQuality, Characteristic)
+
 
     buildServiceList()
     buildCharMap()
