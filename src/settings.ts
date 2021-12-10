f4ec2d835ee50d89a53f4b88f4d3ec128ad5ebf9
Mina Moanes <mina.moanes@outlook.com>
2021-12-10 17:06:01 +0100

    Update info

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomeKitProxy'
+export const PLATFORM_NAME = 'HomekitControl'
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-homekit-proxy'
+export const PLUGIN_NAME = 'homebridge-homekit-control'
